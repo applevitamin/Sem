464e1e0fe66f961b7adee63b968b694241a23560
Vitalyi <119963502+applevitamin@users.noreply.github.com>
2023-01-13 20:42:53 +0300

    Написали программу по вычислению квадрата числа

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат ( умноженное на само себя)
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+// Решение математически
+int num = number * number;
+Console.WriteLine(num);
+Console.WriteLine($"Квадрат числа {number} равен {num}");
+
+// Решение через библиотеку
+int num2 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа {number} равен {num2}");
