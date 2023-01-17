ef20c46a05da78d3059407860a99e01d37888803
Vitalyi <119963502+applevitamin@users.noreply.github.com>
2023-01-17 22:42:37 +0300

    add leasson2_sem2_task5

---
+++ leasson2_sem2_task4/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+Console.Clear();
+Console.WriteLine("Введитечисло");
+int num = int.Parse(Console.ReadLine()!);
+if( num % 7 == 0 || num % 23 == 0) Console.WriteLine("Кратно");
+else {
+    Console.WriteLine("Не кратно!");
+}
---
+++ leasson2_sem2_task5/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого. 
+//● 5, 25 -> да ● -4, 16 -> да ● 25, 5 -> да ● 8,9 -> нет
+
+Console.WriteLine("Введите первое число: ");
+int a1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: "); 
+int a2 = int.Parse(Console.ReadLine());
+
+
+if (a1 == a2 * a2 || a2 == a1 * a1)
+{
+    Console.WriteLine(" да ");
+}
+else
+{
+    Console.WriteLine(" нет ");
+}
