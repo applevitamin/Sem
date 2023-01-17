8cd66c25a8326cc2dcebb39674b6b4e44aa984a1
Vitalyi <119963502+applevitamin@users.noreply.github.com>
2023-01-17 21:21:55 +0300

    add leasson2_sem2_task2

---
+++ leasson2_sem2_task2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+Console.Clear();
+int number = new Random().Next(100, 1000);
+
+int number1 = number / 100;
+int number3 = number % 10;
+
+int result = number1 * 10 + number3;
+
+Console.WriteLine($"Трехзначное число {number}");
+Console.WriteLine($"Из числа {number} -> получаем {result} удалили вторую цифру");
