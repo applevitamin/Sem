c99924cc9bd294acfb9be0a4cb380382beccf4bc
Vitalyi <119963502+applevitamin@users.noreply.github.com>
2023-01-20 20:59:26 +0300

    add sem3_task1/sem3_task2

---
+++ sem3_task1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+Console.Clear();
+Console.WriteLine("Введите X: ");
+int x = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите Y: ");
+int y = int.Parse(Console.ReadLine()!);
+
+if (y > 0)
+{
+    if (x > 0)
+    {
+        Console.WriteLine("Первая четверть");
+    }
+    else
+    {
+        Console.WriteLine("Вторая четверть");
+    }
+}
+
+else
+{
+    if (y > 0)
+    {
+        Console.WriteLine("Третья четверть");
+    }
+    else
+    {
+        Console.WriteLine("Четвертая четверть");
+    }
+}
---
+++ sem3_task2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача No18. Работа в группах
+// Напишите программу, которая по заданному номеру
+// четверти, показывает диапазон возможных координат
+// точек в этой четверти (x и y).
+Console.Clear();
+
+Console.WriteLine("Введите четверть: ");
+int num = int.Parse(Console.ReadLine()!);
+if (num > 0 && num < 5)
+{
+    if (num == 1)
+    {
+        Console.WriteLine("Первая четверть, | X > 0 | Y > 0 |");
+    }
+    else
+    {
+        if (num == 2)
+        {
+            Console.WriteLine("Вторая четверть, | X < 0 | Y > 0 |");
+        }
+        else
+        {
+            if (num == 3)
+            {
+                Console.WriteLine("Третья четверть, | X < 0 | Y < 0 |");
+            }
+            else Console.WriteLine("Четвертая четверть, | X > 0 | Y < 0 |");
+        }
+    }
+
+}
+else Console.WriteLine("Неверное значение!");
